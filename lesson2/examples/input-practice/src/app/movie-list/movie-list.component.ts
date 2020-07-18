5252401574a581c6dff70009b58c86323a546af9
Nicole Carrero <ncarrero18@gmail.com>
2020-07-18 12:44:40 -0400

    completion of lesson examples

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,13 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie(newTitle: string) {
+      if (!this.movies.includes(newTitle)) {
+         this.movies.push(newTitle);  
+      }
+      if (newTitle = "") {
+         return "You need to type a movie title before submitting.";
+      }
+   }
 }
